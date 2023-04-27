78e2079ff11c5f78f0473351cf1cd62b867f1f86
AalguienN <93289662+AalguienN@users.noreply.github.com>
2023-04-27 10:41:04 +0200

    Modificación de nombres

--- ArmInput.cs
+++ ArmInput.cs
@@ -8,10 +8,10 @@ public class ArmInput : MonoBehaviour {
 
     [Header("Inputs")]
 
-    public KeyCode inputForAxis1 = KeyCode.F;
-    public KeyCode inputForAxis2 = KeyCode.J;
-    public KeyCode inputForAxis3 = KeyCode.D;
-    public KeyCode inputForAxis4 = KeyCode.K;
+    public KeyCode inputForSignal1 = KeyCode.F;
+    public KeyCode inputForSignal2 = KeyCode.J;
+    public KeyCode inputForSignal3 = KeyCode.D;
+    public KeyCode inputForSignal4 = KeyCode.K;
 
 
     //Cada señal es equivalente a un eje
@@ -21,8 +21,8 @@ public class ArmInput : MonoBehaviour {
     private static float Signal4;
 
 
-    public enum axis{
-        A1, A2, A3, A4,
+    public enum Signal{
+        S1, S2, S3, S4,
         LBiceps, RBiceps, LTriceps, RTriceps
     }
 
@@ -39,44 +39,44 @@ public class ArmInput : MonoBehaviour {
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetKey(inputForAxis1)) Signal1 = 1; else Signal1 = 0;
-        if (Input.GetKey(inputForAxis2)) Signal2 = 1; else Signal2 = 0;
-        if (Input.GetKey(inputForAxis3)) Signal3 = 1; else Signal3 = 0;
-        if (Input.GetKey(inputForAxis4)) Signal4 = 1; else Signal4 = 0;
+        if (Input.GetKey(inputForSignal1)) Signal1 = 1; else Signal1 = 0;
+        if (Input.GetKey(inputForSignal2)) Signal2 = 1; else Signal2 = 0;
+        if (Input.GetKey(inputForSignal3)) Signal3 = 1; else Signal3 = 0;
+        if (Input.GetKey(inputForSignal4)) Signal4 = 1; else Signal4 = 0;
     }
 
-    public static float GetAxis(string axisName) {
-        switch (axisName) {
-            case "A1":
+    public static float GetSignal(string SignalName) {
+        switch (SignalName) {
+            case "S1":
             case "LBiceps":
                 return Signal1;
-            case "A2":
+            case "S2":
             case "RBiceps":
                 return Signal2;
-            case "A3":
+            case "S3":
             case "LTriceps":
                 return Signal3;
-            case "A4":
+            case "S4":
             case "RTriceps":
                 return Signal4;
         }
         return 0;
     }
-    public static float GetAxis(axis axis)
+    public static float GetSignal(Signal signal)
     {
-        switch (axis)
+        switch (signal)
         {
-            case axis.A1:
-            case axis.LBiceps:
+            case Signal.S1:
+            case Signal.LBiceps:
                 return Signal1;
-            case axis.A2:
-            case axis.RBiceps:
+            case Signal.S2:
+            case Signal.RBiceps:
                 return Signal2;
-            case axis.A3:
-            case axis.LTriceps:
+            case Signal.S3:
+            case Signal.LTriceps:
                 return Signal3;
-            case axis.A4:
-            case axis.RTriceps:
+            case Signal.S4:
+            case Signal.RTriceps:
                 return Signal4;
         }
         return 0;
